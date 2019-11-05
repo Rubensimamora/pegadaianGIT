f65dc5c7014b4e508bc29c3089b68f4ae8ec1dd9
Rubensimamora <rubensimamora@pegdaian.co.id>
2019-11-05 09:54:41 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+package pegadaiangit;
+import java.io.*;
+
+public class main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Version 1");
+		System.out.println("Hello World");
+		
+
+	}
+
+}

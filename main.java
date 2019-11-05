17c9e939a23c26c5c11437808ff3792a5cd6f311
Rubensimamora <rubensimamora@pegdaian.co.id>
2019-11-05 09:57:56 +0700

    Update main.java

--- main.java
+++ main.java
@@ -5,10 +5,9 @@ public class main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Version 1");
+		System.out.println("Version 2");
 		System.out.println("Hello World");
 		
-
 	}
 
 }
